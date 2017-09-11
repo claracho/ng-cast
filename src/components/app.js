cb6620cb00de72f6aed6d4494df056632555dd9c
Hack Reactor Student <student@hackreactor.com>
2017-09-11 15:06:45 -0700

    Render templates of all sub-components

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,11 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+  controller: function() {
+    this.videos = [];
+    this.currentVideo = {};
+    this.selectVideo = function() {};
+    this.searchResults = function() {};
+  },
+  templateUrl: 'src/templates/app.html'
 });
